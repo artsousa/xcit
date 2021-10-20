5ba8c4c81839bea14b13f5045a9512435b707f29
arthur sousa <arthursvrr@gmail.com>
2021-10-20 02:51:22 +0000

    retina config

--- detection/configs/xcit/retinanet/retina_monitors.py
+++ detection/configs/xcit/retinanet/retina_monitors.py
@@ -7,10 +7,10 @@ ann_train = data_root + '/annotations/monitors_train.json'
 
 pclasses = 1
 log_config = dict(
-    interval=50,
+    interval=20,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
+        #dict(type='TensorboardLoggerHook')
     ])
 custom_hooks = [dict(type='NumClassCheckHook')]
 
@@ -27,16 +27,16 @@ runner = dict(type='EpochBasedRunner', max_epochs=100)
 
 fp16 = None
 im_size = (500, 500)
-optimizer_config = dict(grad_clip=None)
 
-img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=False)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+img_norm_cfg = dict(mean=None, std=None, to_rgb=False)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=im_size, keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.0),
     dict(type='Normalize', **img_norm_cfg),
-    # dict(type='Pad', size_divisor=32),
+    #dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
@@ -57,7 +57,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=16,
-    workers_per_gpu=1,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         classes=classes,
@@ -77,13 +77,13 @@ data = dict(
         img_prefix=img_prefix,
         pipeline=test_pipeline))
 
-optimizer = dict(type='AdamW', lr=0.002, betas=(0.9, 0.999), weight_decay=0.05)
+optimizer = dict(type='AdamW', lr=0.0005, betas=(0.9, 0.999), weight_decay=0.0001)
 lr_config = dict(  
-    policy='step', 
+    policy='CosineAnnealing', 
     warmup='linear', 
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
+    warmup_iters=4000,
+    warmup_ratio=0.01,
+    min_lr_ratio=1e-5)
 
 model = dict(
     type='RetinaNet',
@@ -91,12 +91,15 @@ model = dict(
         type='ResNet',
         depth=50,
         num_stages=4,
+        deep_stem=True,
+        zero_init_residual=True,
+        in_channels=3,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
+        frozen_stages=-1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50'),
+        #init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50'),
     ),
     neck=dict(
         type='FPN',
@@ -126,15 +129,16 @@ model = dict(
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
-            loss_weight=1.0),
+            loss_weight=1.0,
+            reduction='sum'),
         loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
     # model training and testing settings
     train_cfg=dict(
         assigner=dict(
             type='MaxIoUAssigner',
-            pos_iou_thr=0.7,
-            neg_iou_thr=0.3,
-            min_pos_iou=0.3,
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.2,
+            min_pos_iou=0.2,
             ignore_iof_thr=-1),
         allowed_border=-1,
         pos_weight=-1,
